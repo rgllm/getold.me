76802c8253a937c307f1fe3f5341ebd0f2b65ad7
Rogério Moreira <r@rgllm.com>
2023-01-11 12:49:49 +0000

    Add Next.js with Turbopack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  experimental: {
+    // Required:
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
